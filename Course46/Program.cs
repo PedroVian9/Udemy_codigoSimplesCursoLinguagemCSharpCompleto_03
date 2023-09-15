43b09ca43c76e53faafc7198d1a5709c2e9ae691
Pedro Macedo Paronetto Faria Viana <pedro.viana@gavresorts.com.br>
2023-09-15 16:31:35 -0300

    Implementação Classe Calculadora

--- Course46/Program.cs
+++ Course46/Program.cs
@@ -1,31 +1,25 @@
-﻿using System;
+﻿using Course46;
+using System;
 using System.Globalization;
+
+//Olhar histórico, pois possuem versões diferentes
 
 namespace PMPFV 
 {
     internal class Program
     {
-        static double Pi = 3.14;
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Entre o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circ = Circunferencia(raio);
-            double volume = Volume(raio);
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
 
             Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Pi: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
-        }
-
-        static double Circunferencia(double r)
-        {
-            return 2.0 * Pi * r;
-        }
-        static double Volume(double r)
-        {
-            return 4.0 / 3.0 * Pi * r * r * r;
-        }
+            Console.WriteLine("Pi: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }  
     }
 }
